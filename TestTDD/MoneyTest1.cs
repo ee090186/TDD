3e803e22c1afc45c9619bce9545951ed1637a5da
NakashimaShuusaku <Shusaku.Nakashima@sony.com>
2023-06-21 15:53:55 +0900

    [update]Francクラスの透過性テスト追加

--- TestTDD/MoneyTest1.cs
+++ TestTDD/MoneyTest1.cs
@@ -20,6 +20,10 @@ namespace TestTDD
                         new Dollar(5)));
             Assert.False(new Dollar(5).Equals(
                          new Dollar(6)));
+            Assert.True(new Franc(5).Equals(
+                        new Franc(5)));
+            Assert.False(new Franc(5).Equals(
+                         new Franc(6)));
         }
         [Fact]
         public void TestFrancMultiplication()
